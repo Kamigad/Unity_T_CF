74315d00166006def5abaec7b43a4f35ed66ed87
Camilo Garcia <camilo971939@gmail.com>
2020-06-25 00:10:02 -0500

    Add cube and move

---
+++ CodigoFacilito-Unity/Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // variable transform con la funcion Rotate se encarga de la rotacion del objeto en 3D
+        // El Time.deltaTime se encarga de que el movimiento por cuadros del objeto sea el mismo en esta maquina como en cualquier maquina que sea descargada
+        transform.Rotate(new Vector3 (0f, 30f, 0f) * Time.deltaTime);
+    }
+}
