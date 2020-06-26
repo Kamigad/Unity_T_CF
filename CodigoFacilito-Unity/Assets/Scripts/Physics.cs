ee5bf0243215e92d36b4e9fc7db6baaa73cef3ed
Camilo Garcia <camilo971939@gmail.com>
2020-06-26 01:05:41 -0500

    Add Física: Tipos de fuerzas II
    Force
    Acceleration

--- CodigoFacilito-Unity/Assets/Scripts/Physics.cs
+++ CodigoFacilito-Unity/Assets/Scripts/Physics.cs
@@ -7,13 +7,13 @@ public class Physics : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Por medio del Rigidbody podremos mover de forma inmediata teniendo en cuenta su masa al gameobject
-        GetComponent<Rigidbody>().AddForce(Vector3.right * 50f, ForceMode.Impulse);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Por medio del Rigidbody podremos mover de forma continua teniendo en cuenta su masa al gameobject
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 50f, ForceMode.Force);
     }
 }
