14fd2aac26633d91f90b061a920ed4559a6f4c8a
Camilo Garcia <camilo971939@gmail.com>
2020-06-26 15:55:50 -0500

    Add Random

--- CodigoFacilito-Unity/Assets/Scripts/Physics.cs
+++ CodigoFacilito-Unity/Assets/Scripts/Physics.cs
@@ -8,7 +8,9 @@ public class Physics : MonoBehaviour
     void Start()
     {
         // uso de fisicas impulsando el objeto de forma inmediata en base a su masa
-        GetComponent<Rigidbody>().AddForce(Vector3.forward * 100f, ForceMode.Impulse);
+        // por medio de la funcion Random, podremos elegir entre un numero minimo y un numero maximo aleatoriamente, siendo util en float y enteros
+        float x = Random.Range(-1f, 1f);
+        GetComponent<Rigidbody>().AddForce(new Vector3(x,0f,1f) * 100f, ForceMode.Impulse);
     }
 
 }
