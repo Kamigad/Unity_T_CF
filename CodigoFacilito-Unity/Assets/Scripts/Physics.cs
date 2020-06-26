2b57846e8143bab63601556e9879a59f2513aeb8
Camilo Garcia <camilo971939@gmail.com>
2020-06-26 00:58:02 -0500

    Add Física: Tipos de fuerzas
    Impulse
    VelocityChange

---
+++ CodigoFacilito-Unity/Assets/Scripts/Physics.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Por medio del Rigidbody podremos mover de forma inmediata teniendo en cuenta su masa al gameobject
+        GetComponent<Rigidbody>().AddForce(Vector3.right * 50f, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
