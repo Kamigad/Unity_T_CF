7930e8d7c276536e1cb1ec29007fb65192bdaca0
Camilo Garcia <camilo971939@gmail.com>
2020-06-26 16:01:42 -0500

    Add Instanciando Prefabs II

--- CodigoFacilito-Unity/Assets/Scripts/Spawn.cs
+++ CodigoFacilito-Unity/Assets/Scripts/Spawn.cs
@@ -9,15 +9,20 @@ public class Spawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // por medio de esta linea de code se utiliza para que aparezca el gameobject que definimos en el inpector. Haciendo uso de el gameobject, la posicion en donde
-        // queremos que este aparezca y la rotacion que queremos que tenga. Al usar .position y .rotation indican la posicion y la rotacion del objeto vacio que hay en la
-        // escena.
-        Instantiate(prefab, transform.position, transform.rotation);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // por medio de esta linea de code se utiliza para que aparezca el gameobject que definimos en el inpector. Haciendo uso de el gameobject, la posicion en donde
+        // queremos que este aparezca y la rotacion que queremos que tenga. Al usar .position y .rotation indican la posicion y la rotacion del objeto vacio que hay en la
+        // escena.
+        // siempre y cuando oprima la barra espaciadora aparecera mi prefab en la escena
+        // agregando el GetkeyUp, la accion de aparecer se ejecutara siempre y cuando yo suelte la tecla
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
     }
 }
