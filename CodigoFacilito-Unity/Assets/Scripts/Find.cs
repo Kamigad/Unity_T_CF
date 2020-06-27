db5dc7066de7a4cbbac615e450c5313aaeb7d149
Camilo Garcia <camilo971939@gmail.com>
2020-06-26 19:37:33 -0500

    Add Buscando objetos II

--- CodigoFacilito-Unity/Assets/Scripts/Find.cs
+++ CodigoFacilito-Unity/Assets/Scripts/Find.cs
@@ -7,14 +7,14 @@ public class Find : MonoBehaviour
     // Awake: funcion de monobehavior que se ulitiliza para encontrar objetos y almacenar todos los componenetes. Se ejecuta antes del Start
     private void Awake()
     {
-        // forma de encontrar la camara por medio del nombre
-        // forma de encontrar la camara por medio del Tag
-        // forma de econtrar la camara principal por medio de la clase Carama y la variable main
-        // Debug.Log: se encarga de imprimir un mensaje en la consola, en este caso, la position de la camara
-        GameObject camara = GameObject.Find("Main Camera");
-        camara = GameObject.FindGameObjectWithTag("MainCamera");
-        GameObject mCamara = Camera.main.gameObject;
-        Debug.Log(camara.transform.position);
-        Debug.Log(mCamara.transform.position);
+        // mediante la clase GameObject y el metodo FindGameObjectsWithTag, usaremos un tag para que nos retorne un arreglo de GameObjects
+        // al hacerlo con el metodo en singular al ser un grupo de objetos con el mismo tag este tomara el primero que encuentre
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Objeto");
+
+        // Despues interamos en cada uno de los GameObjects por medio del foreach, para finalmente imprimir la posicion de cada GameObject
+        foreach(GameObject go in objects)
+        {
+            Debug.Log(go.transform.position);
+        }
     }
 }
