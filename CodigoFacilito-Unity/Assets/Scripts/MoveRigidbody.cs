7e339b119eb80afb7269bfe7f5220ab13fe54696
Camilo Garcia <camilo971939@gmail.com>
2020-06-28 12:55:58 -0500

    Add Moviendo objetos con física

---
+++ CodigoFacilito-Unity/Assets/Scripts/MoveRigidbody.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRigidbody : MonoBehaviour
+{
+    // Variable global para darle velocidad al gameobject
+    public float speed;
+    
+    // funcion usada para el manejo de gravedad
+    private void FixedUpdate()
+    {
+        // Forma de realizar el movimiento simple del objeto haciendo uso del Rigidbody
+        if(Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.forward * speed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.back * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.right * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.left * speed * Time.deltaTime);
+        }
+    }
+}
